aa9dcf57baf69a1e44bed18dc0246ce0a7620984
Ansh Chandnani <ansh.chandnani@gmail.com>
2025-09-21 22:14:29 -0700

    nvim: clean up lsp and autocomplete configs

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,35 +1,49 @@
--- This file defines the autocompletion plugin
+-- This file is solely responsible for the autocompletion ecosystem.
+-- It configures the main completion engine (nvim-cmp), the snippet engine (LuaSnip),
+-- and all the sources that provide completion suggestions.
 
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter", -- Load the plugin only when we enter insert mode
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp", -- For LSP completions
+            "hrsh7th/cmp-buffer", -- For completions from the current buffer
+            "hrsh7th/cmp-path", -- For file path completions
+            "saadparwaiz1/cmp_luasnip", -- For snippet completions
+            "L3MON4D3/LuaSnip", -- snippet engine, nvim-cmp uses this to expand snippets
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                -- keybindings for the completion menu
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                }),
+                -- This defines the order of completion sources.
+                -- nvim-cmp will get suggestions from nvim_lsp and luasnip first,
+                -- then from the buffer and file paths.
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            })
+        end,
     },
-    config = function()
-        local cmp = require("cmp")
-        cmp.setup({
-            -- Your cmp setup table from before
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-j>"] = cmp.mapping.select_next_item(),
-                ["<C-k>"] = cmp.mapping.select_prev_item(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-            }, {
-                { name = "buffer" },
-                { name = "path" },
-            }),
-        })
-    end,
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,48 +1,24 @@
--- This file configures the Language Server Protocol (LSP) for code intelligence.
+-- This file is solely responsible for the Language Server Protocol (LSP) ecosystem.
+-- It ensures that language servers are installed (Mason), that they are configured to work with
+-- Neovim's LSP client (mason-lspconfig), and that the core LSP client itself is set up (nvim-lspconfig).
 
 return {
-    -- LSP Configuration
     {
         "neovim/nvim-lspconfig",
         dependencies = {
-            "williamboman/mason.nvim",
+            {
+                "williamboman/mason.nvim",
+                config = function()
+                    require("mason").setup()
+                end,
+            },
             "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-            "folke/neodev.nvim",
+            { "folke/neodev.nvim", opts = {} },
         },
         config = function()
-            -- Setup mason for auto-installing LSP servers
-            require("mason").setup({
-                ensure_installed = {
-                    "bzl",
-                    "shellcheck",
-                    "stylua",
-                },
-            })
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "bashls",        -- Shell
-                    "buf_ls",        -- Protobuf
-                    "clangd",        -- C/C++
-                    "gopls",         -- Go
-                    "marksman",      -- Markdown
-                    "pyright",       -- Python
-                    "rust_analyzer", -- Rust
-                    "ts_ls",         -- TypeScript/JavaScript
-                },
-            })
-
-            -- Setup neodev for Neovim Lua development
-            require("neodev").setup()
-
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-            -- Keymaps for when LSP attaches
             local on_attach = function(client, bufnr)
                 local opts = { buffer = bufnr, remap = false }
                 local map = vim.keymap.set
-
                 map("n", "K", vim.lsp.buf.hover, opts)                 -- Show documentation for symbol under cursor
                 map("n", "gd", vim.lsp.buf.definition, opts)           -- Go to definition
                 map("n", "gD", vim.lsp.buf.declaration, opts)          -- Go to declaration
@@ -56,87 +32,51 @@ return {
                 map("n", "<leader>f", vim.lsp.buf.format, opts)        -- Format
             end
 
-            -- Configure each LSP server
-            local servers = {
-                bashls = {},
-                buf_ls = {},
-                clangd = {},
-                gopls = {
-                    settings = {
-                        gopls = {
-                            analyses = {
-                                unusedparams = true,
+            -- Capabilities are a way for the client (Neovim) to tell the server
+            -- what features it supports. We get the default capabilities and then enhance
+            -- them with the capabilities from nvim-cmp (for autocompletion).
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            -- This block uses the modern `setup_handlers` method to configure servers.
+            -- It automatically sets up any server that is installed via Mason and listed below.
+            require("mason-lspconfig").setup_handlers({
+                -- The default handler for each server
+                function(server_name)
+                    require("lspconfig")[server_name].setup({
+                        on_attach = on_attach,
+                        capabilities = capabilities,
+                    })
+                end,
+
+                -- For servers that need custom settings, we can override the default handler.
+                ["gopls"] = function()
+                    require("lspconfig").gopls.setup({
+                        on_attach = on_attach,
+                        capabilities = capabilities,
+                        settings = {
+                            gopls = {
+                                analyses = { unusedparams = true },
+                                staticcheck = true,
                             },
-                            staticcheck = true,
                         },
-                    },
-                },
-                marksman = {},
-                pyright = {
-                    settings = {
-                        python = {
-                            analysis = {
-                                typeCheckingMode = "basic",
-                                -- For virtual environments
-                                autoSearchPaths = true,
-                                useLibraryCodeForTypes = true,
+                    })
+                end,
+                ["pyright"] = function()
+                    require("lspconfig").pyright.setup({
+                        on_attach = on_attach,
+                        capabilities = capabilities,
+                        settings = {
+                            python = {
+                                analysis = {
+                                    typeCheckingMode = "basic",
+                                    -- For virtual environments
+                                    autoSearchPaths = true,
+                                    useLibraryCodeForTypes = true,
+                                },
                             },
                         },
-                    },
-                },
-                ts_ls = {},
-            }
-
-            for server, config in pairs(servers) do
-                config.capabilities = capabilities
-                config.on_attach = on_attach
-                lspconfig[server].setup(config)
-            end
-
-            -- Bazel support (using Starlark LSP)
-            lspconfig.starlark_rust.setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-            })
-        end,
-    },
-
-    -- Autocompletion
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-        },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-k>"] = cmp.mapping.select_prev_item(),
-                    ["<C-j>"] = cmp.mapping.select_next_item(),
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                }, {
-                    { name = "buffer" },
-                    { name = "path" },
-                }),
+                    })
+                end,
             })
         end,
     },
